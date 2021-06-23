be96f6f82db22ebca1e0c0d1ab1ac1fbdbb9970c
TshepoDavid Molefe <tshepomolefeda@gmail.com>
2021-06-23 22:34:58 +0200

    Add todo item class

---
+++ src/add_todo.js
@@ -0,0 +1,11 @@
+class TodoItem {
+ constructor(title, description, dueDate, proirity, task) {
+  this.title = title;
+  this.description = description;
+  this.dueDate = dueDate;
+  this.proirity = proirity;
+  this.task = task;
+ }
+}
+
+export default TodoItem;
